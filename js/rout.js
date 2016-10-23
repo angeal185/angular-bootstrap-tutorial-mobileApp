f6cc1bb48b62ad7ecac902ae43d6b18b1e5c2d0a
Angeal185 <beneaves01@hotmail.com>
2016-10-24 00:53:26 +0800

    Add files via upload

---
+++ js/rout.js
@@ -0,0 +1,155 @@
+var app = angular.module('myApp', ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+	.when("/", {
+        templateUrl : "partials/1.html",
+		controller : "myCtrl"
+    })
+    .when("/1", {
+        templateUrl : "partials/1.html",
+		controller : "myCtrl"
+    })
+	.when("/2", {
+        templateUrl : "partials/2.html",
+		controller : "myCtrl"
+	})
+	.when("/3", {
+        templateUrl : "partials/3.html",
+		controller : "myCtrl"
+	})
+	.when("/4", {
+        templateUrl : "partials/4.html",
+		controller : "myCtrl"
+	})
+	.when("/5", {
+        templateUrl : "partials/5.html",
+		controller : "myCtrl"
+	})
+		.when("/6", {
+        templateUrl : "partials/6.html",
+		controller : "myCtrl"
+	})
+		.when("/7", {
+        templateUrl : "partials/7.html",
+		controller : "myCtrl"
+	})
+		.when("/8", {
+        templateUrl : "partials/8.html",
+		controller : "myCtrl"
+	})
+		.when("/9", {
+        templateUrl : "partials/9.html",
+		controller : "myCtrl"
+	})
+		.when("/10", {
+        templateUrl : "partials/10.html",
+		controller : "myCtrl"
+	})
+		.when("/11", {
+        templateUrl : "partials/11.html",
+		controller : "myCtrl"
+	})
+		.when("/12", {
+        templateUrl : "partials/12.html",
+		controller : "myCtrl"
+	})
+		.when("/13", {
+        templateUrl : "partials/13.html",
+		controller : "myCtrl"
+	})
+		.when("/14", {
+        templateUrl : "partials/14.html",
+		controller : "myCtrl"
+	})
+		.when("/15", {
+        templateUrl : "partials/15.html",
+		controller : "myCtrl"
+	})
+		.when("/16", {
+        templateUrl : "partials/16.html",
+		controller : "myCtrl"
+	})
+		.when("/17", {
+        templateUrl : "partials/17.html",
+		controller : "myCtrl"
+	})
+		.when("/18", {
+        templateUrl : "partials/18.html",
+		controller : "myCtrl"
+	})
+		.when("/19", {
+        templateUrl : "partials/19.html",
+		controller : "myCtrl"
+	})
+		.when("/20", {
+        templateUrl : "partials/20.html",
+		controller : "myCtrl"
+	})
+		.when("/21", {
+        templateUrl : "partials/21.html",
+		controller : "myCtrl"
+	})
+		.when("/22", {
+        templateUrl : "partials/22.html",
+		controller : "myCtrl"
+	})
+		.when("/23", {
+        templateUrl : "partials/23.html",
+		controller : "myCtrl"
+	})
+		.when("/24", {
+        templateUrl : "partials/24.html",
+		controller : "myCtrl"
+	})
+		.when("/25", {
+        templateUrl : "partials/25.html",
+		controller : "myCtrl"
+	})
+		.when("/26", {
+        templateUrl : "partials/26.html",
+		controller : "myCtrl"
+	})
+		.when("/27", {
+        templateUrl : "partials/27.html",
+		controller : "myCtrl"
+	})
+		.when("/28", {
+        templateUrl : "partials/28.html",
+		controller : "myCtrl"
+	})
+		.when("/29", {
+        templateUrl : "partials/29.html",
+		controller : "myCtrl"
+	})
+		.when("/30", {
+        templateUrl : "partials/30.html",
+		controller : "myCtrl"
+	})
+	.when("/31", {
+        templateUrl : "partials/31.html",
+		controller : "myCtrl"
+    });
+});
+
+
+
+app.controller('myCtrl', function($scope) {
+    $scope.jsv = "javascript:void(0)";
+});
+
+
+
+    
+
+
+
+function cdp() {
+    window.open("https://codepen.io/angeal185/");
+}
+function lnd() {
+    window.open("https://au.linkedin.com/in/ben-eaves-996991125");
+}
+function gth() {
+    window.open("https://angeal185.github.io");
+}
